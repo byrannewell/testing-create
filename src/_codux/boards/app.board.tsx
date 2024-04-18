3ea28f507e2eb1830e503a72ae633bb0d0d5cbb1
Codux <codux@codux.com>
2024-04-18 12:24:33 -0700

    Test

--- src/_codux/boards/app.board.tsx
+++ src/_codux/boards/app.board.tsx
@@ -6,4 +6,7 @@ export default createBoard({
     name: 'App',
     Board: () => <App product={RUBBER_DUCKY} user={SCROOGE} />,
     isSnippet: true,
+    environmentProps: {
+        canvasWidth: 1108,
+    },
 });
